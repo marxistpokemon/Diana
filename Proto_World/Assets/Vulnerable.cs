ddc0a16b04e0a33eeec7f088e8306dc898c43e4d
marxistpokemon <enric.llagostera@gmail.com>
2014-03-03 11:42:16 -0300

    Signals library imported.

--- Proto_World/Assets/Vulnerable.cs
+++ Proto_World/Assets/Vulnerable.cs
@@ -4,10 +4,5 @@ using System.Collections;
 public class Vulnerable : BaseCharacteristic {
 
 	public float health;
-
-	public void Update(){
-		if(health < 0){
-			base.worldObj.Destroy();
-		}
-	}
+	
 }
